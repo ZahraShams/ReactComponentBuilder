21afd52cf5903d48ceda68e80987afaa1347dc9a
zahra <zahrashmousavi@gmail.com>
2023-09-06 21:02:02 +0330

    add node lookup for factory

---
+++ src/utils/NodeLookup.ts
@@ -0,0 +1,30 @@
+import {
+    BoxNode,
+    ButtonNode,
+    H1Node,
+    H2Node,
+    LinkNode,
+    ListNode,
+    ModalNode,
+    ParagraphNode,
+  } from './TreeNode';
+  type AllowedKeys =
+    | 'H1Component'
+    | 'H2Component'
+    | 'ListComponent'
+    | 'ButtonComponent'
+    | 'LinkComponent'
+    | 'ParagraphComponent'
+    | 'BoxComponent'
+    | 'ModalComponent';
+  
+export  const NodeLookup: { [K in AllowedKeys]: TreeNode } = {
+    H1Component: H1Node,
+    H2Component: H2Node,
+    ListComponent: ListNode,
+    ButtonComponent: ButtonNode,
+    ParagraphComponent: ParagraphNode,
+    LinkComponent: LinkNode,
+    ModalComponent: ModalNode,
+    BoxComponent: BoxNode,
+  };
